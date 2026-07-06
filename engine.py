0b831c1159acf2f17544483d9de86ea4457ad4c6
Marisol Yake <marisol.yake@outlook.com>
2026-07-06 15:38:19 -0600

    add script for logic

---
+++ engine.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Func: Fetch edcaps calculator (JS directly embedded in HTML)
+# Use streamlit cache to only refresh the HTML calculator form once per week
+
+# Func: Extract HTML Form - Repayment Calculator
+
+# Func: Extract JS logic
+
+# Func: Streamlit User Inputs (defined by edcaps form params)
+#           -> ([IDR Plans], Traditional Payment Plan) monthly payment amounts
