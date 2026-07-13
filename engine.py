7293990a709c1db70dafccf42b9fe1672b73f1c5
Marisol Yake <marisol.yake@outlook.com>
2026-07-13 14:42:41 -0600

    extract js calc logic and use in comparison calcs

--- engine.py
+++ engine.py
@@ -1,43 +1,86 @@
 # Defines internal application logic
 import streamlit as st
 import bs4
-import js2py
 import requests
-from typing import Union
+import pythonmonkey as pm
+import typing
 
 
+class JavascriptNotFoundError(Exception):
+    pass
+
+# TODO: Wrap this in a class - EDCAPCalcFetcher
 # Func: Fetch edcaps calculator (JS directly embedded in HTML)
 # Use streamlit cache to only refresh the HTML calculator form once per week
 # @st.cache_data()
 def fetch_edcaps_HTML():
     ...
 
-
+# TODO: Wrap this in a class - EDCAPCalcFetcher
 # Func: Extract HTML Form - Repayment Calculator
 def extract_calc_HTML_form():
-    ...
+    filepath = "./Repayment-Plan-Calculator-7.6.26.html"
 
+    with open(filepath, "r", encoding="utf-8") as HTML:
+        soup = bs4.BeautifulSoup(HTML, "html.parser")
+        return soup
 
+# TODO: Wrap this in a class - EDCAPCalcFetcher
 # Func: Extract JS logic - Repayment Calculator
-def extract_calc_JS_logic():
-    ...
+def extract_calc_JS_logic(soup: bs4.BeautifulSoup):
+    try:
+        calc_logic = soup.find_all("script")[0].get_text(strip=True)
+        pm.eval(calc_logic)
+
+        # Extract relevant logic from the code
+        calculator_ctx = {
+            "calculateStandardPayment": pm.globalThis.calculateStandardPayment,
+            "calculateICR": pm.globalThis.calculateICR,
+            "calculateIBR": pm.globalThis.calculateIBR,
+            "calculatePAYE": pm.globalThis.calculatePAYE,
+            "calculateRAP": pm.globalThis.calculateRAP,
+        }
+
+    except:
+        raise JavascriptNotFoundError("Javascript logic not found. Potentially extracted the wrong HTML file or server blocked this script.")
+
+    return calculator_ctx
 
 
-def calculate_edcap_results():
-    # Take user inputs from streamlit
-    # Use inputs in edcaps calculator
-    # Submit and retrieve results
-    # Return all results
-    return ["trad", "ibr", "icr", "paye", "rap"]
-
-
-def calculate_REPAYE_plan():
+# TODO: Wrap this in a class - LoanRepayCalculator
+def calculateREPAYE(ibr: float):
     # Take necessary user inputs
     # Calculate REPAYE monthly payment plan amount
     # Return result
-    return ["repaye"]
+    return ibr * 0.666
 
 
+# TODO: Wrap this in a class - LoanRepayCalculator
+def calculate_all_plans(ctx: dict):
+    # TODO: Get variables from st.session_state
+    # Test variables:
+    balance = 10_000
+    interest = 7.34
+    agi = 30_000
+    household = 1
+    state = "contiguous"
+    dependents = 0
+    borrowerType = "new"
+
+    # Calculate loan payment amounts based on EDCAP functions
+    ibr = ctx["calculateIBR"](balance, interest, agi, household, state, borrowerType).monthlyPayment
+    icr = ctx["calculateICR"](balance, interest, agi, household, state).monthlyPayment
+    paye = ctx["calculatePAYE"](balance, interest, agi, household, state).monthlyPayment
+    repaye = calculateREPAYE(ibr)
+    rap = ctx["calculateRAP"](agi, dependents).monthlyPayment
+    std = ctx["calculateStandardPayment"](balance, interest, years = 10)
+
+    print(f"{ibr=:,.2f}\n{icr=:,.2f}\n{paye=:,.2f}\n{repaye=:,.2f}\n{rap=:,.2f}\n{std=:,.2f}")
+    
+    return ibr, icr, paye, repaye, rap, std
+
+
+# TODO: Wrap this in a class - LoanRepayCalculator
 # Func: Streamlit User Inputs (defined by edcaps form params)
 #           -> ([IDR Plans], Traditional Payment Plan) monthly payment amounts
 def display_diff(display_value: str, *, percent_diff: float) -> None:
@@ -57,19 +100,26 @@ def display_diff(display_value: str, *, percent_diff: float) -> None:
     else:
         st.metric("**Total Difference**", value=display_value, border=True)
 
-
+# TODO: Wrap this in a class - LoanRepayCalculator
 def calculate_comparisons() -> None:
-    trad, ibr, icr, paye, rap = calculate_edcap_results()
-    repaye = calculate_REPAYE_plan()
+    # TODO: Refactor so this isn't run every time the calculation is run
+    # Probably going to store soup and ctx as class attributes to share state
+    soup = extract_calc_HTML_form()
+    ctx = extract_calc_JS_logic(soup)
+    ibr, icr, paye, repaye, rap, std = calculate_all_plans(ctx)
 
     user_reported_est = 350
-    selected_plan = st.session_state.servicer_estimate  # TODO: Use something to correspond selected_plan to this, probably case statement or dict
+    selected_plan = st.session_state.servicer_estimate
     est_difference = user_reported_est - selected_plan
     est_percent_diff = (est_difference) / selected_plan
 
     display_diff(f"${abs(est_difference)} ({est_percent_diff:0,.0%})", percent_diff=est_percent_diff)
 
-
 # Func: Create popup if there is a flagged difference
 # TODO: Make sure it pops-up only once per session (How?)
 # NOTE: Maybe make this a button, so instead of worrying about implementation, users can opt-in to share
+
+if __name__=="__main__":
+    soup = extract_calc_HTML_form()
+    ctx = extract_calc_JS_logic(soup)
+    calculate_all_plans(ctx)
